2604d395b478e3d9ac2fa20f24dace60bdb46c72
Ulil155 <weedabshor@gmail.com>
2021-05-22 22:29:36 +0700

    update

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layout0155')
+@section('judul','Contact')
+@section('isi')
+
+
+<p>Nama         : Moch. Ulil Abshor</p>
+<p>Nomer        : 081331948232</p>
+<p>Email        : weedabshor@gmail.com</p>
+<p>Sosial Media : @ndeloshor</p>
+<img src="{{url('img1.jpeg')}}">
+@endsection
---
+++ resources/views/home0155.blade.php
@@ -0,0 +1,5 @@
+@extends('layout0155')
+@section('judul','Home')
+@section('isi')
+<H1>Praktikum Pengembangan Teknologi Web</H1>
+@endsection
